46cb2c7691e5f308302893fd936768d8b01bd4bf
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2018-06-17 13:46:21 +0300

    View add @lang method

--- System/Libs/View/View.php
+++ System/Libs/View/View.php
@@ -5,7 +5,7 @@
  *
  * Author 	: Yusuf Kaan Karakaya
  * Web 		: http://theykk.net
-  *
+ *
  * Github	: http://github.com/theykk/fw
  * License	: MIT
  *
@@ -18,49 +18,58 @@ use Windwalker\Edge\Loader\EdgeFileLoader;
 
 class View
 {
-	// Active Theme
-	private $theme = null;
+    // Active Theme
+    private $theme = null;
 
-	/**
-	 * Render View File
-	 *
-	 * @param string $file
-	 * @param array $vars
-	 * @param boolean $cache
-	 * @return void
-	 */
-	public function render($file, $vars = [], $cache = false)
-	{
-		$paths 	= [APP_DIR . 'Views'];
+    /**
+     * Render View File
+     *
+     * @param string $file
+     * @param array $vars
+     * @param boolean $cache
+     * @return void
+     * @throws \Windwalker\Edge\Exception\EdgeException
+     */
+    public function render($file, $vars = [], $cache = false)
+    {
+        $paths 	= [APP_DIR . 'Views'];
 
-		$loader = new EdgeFileLoader($paths);
-		$loader->addFileExtension('.blade.php');
+        $loader = new EdgeFileLoader($paths);
+        $loader->addFileExtension('.blade.php');
 
-		if ($cache === false)
-			$edge = new Edge($loader);
-		else
-			$edge = new Edge($loader, null, new EdgeFileCache(APP_DIR . '/Storage/Cache'));
+        if ($cache === false)
+            $edge = new Edge($loader);
+        else
+            $edge = new Edge($loader, null, new EdgeFileCache(APP_DIR . '/Storage/Cache'));
 
-		if (is_null($this->theme))
-			echo $edge->render($file, $vars);
-		else
-			echo $edge->render($this->theme . '.' . $file, $vars);
-	}
+        $compiler = $edge->getCompiler();
 
-	/**
-	 * Set Activated Theme
-	 *
-	 * @param string $theme
-	 * @return $this
-	 */
-	public function theme($theme)
-	{
-		if (file_exists(APP_DIR . 'Views/' . $theme)) {
-			$this->theme = $theme;
-		} else {
-			throw new \System\Libs\Exception\ExceptionHandler("Hata", "Tema dizini bulunamadı. { $theme }");
-		}
+        $compiler->directive('lang', function ($expression)
+        {
+            return "<?php echo __$expression; ?>";
+        });
 
-		return $this;
-	}
+        if (is_null($this->theme))
+            echo $edge->render($file, $vars);
+        else
+            echo $edge->render($this->theme . '.' . $file, $vars);
+    }
+
+    /**
+     * Set Activated Theme
+     *
+     * @param string $theme
+     * @return $this
+     * @throws \Exception
+     */
+    public function theme($theme)
+    {
+        if (file_exists(APP_DIR . 'Views/' . $theme)) {
+            $this->theme = $theme;
+        } else {
+            throw new \System\Libs\Exception\ExceptionHandler("Hata", "Tema dizini bulunamadı. { $theme }");
+        }
+
+        return $this;
+    }
 }
