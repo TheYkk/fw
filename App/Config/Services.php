f9572587cd13a482dd944c1a572b72dc7d53d9d2
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2018-07-10 13:03:07 +0300

    service beutify

--- App/Config/Services.php
+++ App/Config/Services.php
@@ -27,23 +27,23 @@ return [
 		'Model'				=> System\Libs\Database\Model::class,
 
         //not used too much
-        //        'Pagination'		=> System\Libs\Pagination\Pagination::class,
+        //        'Pagination'		    => System\Libs\Pagination\Pagination::class,
         //        'Event'				=> System\Libs\Event\Event::class,
-        //        'Upload'			=> System\Libs\Upload\Upload::class,
+        //        'Upload'			    => System\Libs\Upload\Upload::class,
         //        'Benchmark'			=> System\Libs\Benchmark\Benchmark::class,
         //        'Image'				=> System\Libs\Image\Image::class,
         //        'Hash'				=> System\Libs\Hashing\Hash::class,
-        //        'Jwt'				=> System\Libs\Http\Jwt::class,
+        //        'Jwt'				    => System\Libs\Http\Jwt::class,
         //        'Curl'				=> System\Libs\Http\Curl::class,
-        //        'Restful'			=> System\Libs\Http\Restful::class,
-        //        'Validation'		=> System\Libs\Validation\Validation::class,
+        //        'Restful'			    => System\Libs\Http\Restful::class,
+        //        'Validation'		    => System\Libs\Validation\Validation::class,
         //        'Mail'				=> System\Libs\Mail\Mail::class,
         //        'Html'				=> System\Libs\Html\Html::class,
         //        'Form'				=> System\Libs\Html\Form::class,
         //        'Date'				=> System\Libs\Date\Date::class,
-        //        'CDN'				=> System\Libs\CDN\CDN::class,
+        //        'CDN'				    => System\Libs\CDN\CDN::class,
         //        'Cache'				=> System\Libs\Cache\Cache::class,
-        //        'Log'				=> System\Libs\Log\Log::class,
+        //        'Log'				    => System\Libs\Log\Log::class,
 	],
 
 	/**
@@ -62,19 +62,19 @@ return [
 
         //not used too much
         //        'Event'				=> System\Facades\Event::class,
-        //        'Pagination'		=> System\Facades\Pagination::class,
-        //        'Upload'			=> System\Facades\Upload::class,
+        //        'Pagination'		    => System\Facades\Pagination::class,
+        //        'Upload'			    => System\Facades\Upload::class,
         //        'Image'				=> System\Facades\Image::class,
         //        'Hash'				=> System\Facades\Hash::class,
         //        'Benchmark'			=> System\Facades\Benchmark::class,
-        //        'Jwt'				=> System\Facades\Jwt::class,
+        //        'Jwt'				    => System\Facades\Jwt::class,
         //        'Cache'				=> System\Facades\Cache::class,
-        //        'Log'				=> System\Facades\Log::class,
-        //        'Validation'		=> System\Facades\Validation::class,
+        //        'Log'				    => System\Facades\Log::class,
+        //        'Validation'		    => System\Facades\Validation::class,
         //        'Curl'				=> System\Facades\Curl::class,
-        //        'Restful'			=> System\Facades\Restful::class,
+        //        'Restful'			    => System\Facades\Restful::class,
         //        'Date'				=> System\Facades\Date::class,
-        //        'CDN'				=> System\Facades\CDN::class,
+        //        'CDN'				    => System\Facades\CDN::class,
         //        'Mail'				=> System\Facades\Mail::class,
         //        'Html'				=> System\Facades\Html::class,
         //        'Form'				=> System\Facades\Form::class,
@@ -85,7 +85,6 @@ return [
 	 */
 	'middlewares'	=> [
 		'default'	=> [],
-
 		'manual'	=> []
 	],
 
